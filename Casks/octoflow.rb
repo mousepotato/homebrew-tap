3b21f4ac8865950af7aee7971b0056aab239ebdb
mousepotato <mousepotato@users.noreply.github.com>
2026-04-28 11:34:13 -0700

    chore: bump OctoFlow to 1.0.0 with real sha256

--- Casks/octoflow.rb
+++ Casks/octoflow.rb
@@ -1,6 +1,6 @@
 cask "octoflow" do
   version "1.0.0"
-  sha256 "PLACEHOLDER"
+  sha256 "210c9b18b4f18dd4df803c4b1cc27b5e6a6655e293b45fc2d4ca5da59ba0acb5"
 
   url "https://github.com/mousepotato/octoflow-release/releases/download/v#{version}/OctoFlow-#{version}.dmg"
   name "OctoFlow"
